cc4a690a222cc2cfa8cd5814b641b65896c1d449
Dikshant Katwal <dikshantktwl321@gmail.com>
2025-11-16 11:27:34 +0545

    chat ui fix

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,8 @@
 "use client";
-import TextField from "@/components/TextField";
 import { Button } from "@/components/ui/button";
 import { useWebSocket } from "@/hooks/useWebSocket";
 import ThemeToggle from "@/theme/theme-toggle";
-import { User } from "lucide-react";
+import { Send, User } from "lucide-react";
 import { useEffect, useRef, useState } from "react";
 
 export default function Chat() {
@@ -67,7 +66,7 @@ export default function Chat() {
 		<div className="flex justify-center items-center p-2">
 			<div className="border grid grid-cols-[12rem_20rem] h-96 rounded-2xl shadow-2xl py-2 px-1">
 				<div className="border-r overflow-hidden flex flex-col p-1 ">
-					<span className="font-semibold font-oswald border-b ">
+					<span className="font-semibold h-7 font-oswald border-b ">
 						Messages
 					</span>
 					<div className="py-2 overflow-y-auto flex flex-col gap-2">
@@ -75,7 +74,7 @@ export default function Chat() {
 							connections?.map((connection: any, i: number) => (
 								<div key={i} onClick={
 									() => handleConnectionConnect(connection.connected_with.username)
-								} className="flex items-center gap-2 p-2 border rounded-md hover:bg-accent">
+								} className={`flex items-center gap-2 p-2 border rounded-md hover:bg-accent ${connection.connected_with.username === recipientId && 'bg-active-background'}`}>
 									<User className="size-7 bg-background rounded-full p-[5px]" />
 									<span className="font-nunito text-sm text-nowrap ">
 										{connection.connected_with.username}
@@ -83,38 +82,28 @@ export default function Chat() {
 								</div>
 							))
 						}
-						{/* <div className="flex items-center gap-2 p-2 border rounded-md hover:bg-accent">
-							<User className="size-7 bg-background rounded-full p-[5px]" />
-							<span className="font-nunito text-sm text-nowrap ">
-								Sus
-							</span>
-						</div> */}
 					</div>
 				</div>
 
-				<div className=" overflow-hidden flex flex-col p-1">
-					<div className="flex items-center justify-between border-b ">
+				<div className=" overflow-hidden grid grid-rows-[1.75rem_1fr_3rem] p-1">
+					<div className="border-b flex justify-between">
 						<h2 className="font-normal font-nunito underline">{recipientId || ""} </h2>
 						<h2 className="font-semibold">{userData.username} <ThemeToggle /></h2>
 					</div>
-
-					<div className=" overflow-y-auto px-3 py-2 space-y-2">
+					<div className="scrollbar-hide overflow-y-auto px-3 pt-2 space-y-2 ">
 						{messages.map((msg: any, i) => {
 							const isUser = msg.username === userData.username;
 							return (
-								<div
-									key={i}
-									className={`flex ${isUser ? "justify-end" : "justify-start"}`}
-								>
+								<div key={i} className={`flex ${isUser ? "justify-end" : "justify-start"}`}>
 									<div
-										className={`max-w-[70%] rounded-2xl px-3 py-2 text-sm ${isUser
+										className={`max-w-[70%] rounded-2xl px-2 py-1 text-sm ${isUser
 											? "bg-blue-500 text-white rounded-br-none"
 											: "bg-gray-200 text-gray-800 rounded-bl-none"
 											}`}
 									>
 										<p>{msg.message}</p>
 										{msg.timestamp && (
-											<span className="text-[10px] block mt-1 opacity-70 text-right">
+											<span className="text-[10px] block opacity-70 text-right">
 												{new Date(msg.timestamp).toLocaleTimeString([], {
 													hour: "2-digit",
 													minute: "2-digit",
@@ -123,115 +112,28 @@ export default function Chat() {
 										)}
 									</div>
 								</div>
-							);
+							)
 						})}
 						<div ref={messageEndRef} />
-					</div>
 
-					<div className="border-t px-3 py-2 flex items-center gap-2">
-						{!socketUrl ? (
-							<>
-								<TextField
-									label="Recipient ID"
-									type="text"
-									onChange={(e) => setRecipientId(e.target.value)}
-								/>
-								<Button onClick={handleConnect}>Start Chat</Button>
-							</>
-						) : (
-							<>
-								<input
-									type="text"
-									placeholder="Type a message..."
-									value={message}
-									onChange={(e) => setMessage(e.target.value)}
-									onKeyDown={(e) => e.key === "Enter" && handleMessageSend()}
-									className="flex-1 rounded-full border border-gray-300 px-4 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-blue-400"
-								/>
-								<Button onClick={handleMessageSend}>Send</Button>
-							</>
-						)}
 					</div>
+					{!socketUrl ? (
+						<div className="shadow-2xl h-full flex items-center justify-center">
+						</div>
+					) : (
+						<div className="bg-background border-t  py-2 flex items-center gap-1 ">
+							<input
+								type="text"
+								placeholder="Type a message..."
+								value={message}
+								onChange={(e) => setMessage(e.target.value)}
+								onKeyDown={(e) => e.key === "Enter" && handleMessageSend()}
+								className="flex-1 rounded-md border px-4 py-2 text-sm outline-none"
+							/>
+							<Button variant={"outline"} className=" cursor-pointer" onClick={handleMessageSend}><Send /></Button>
+						</div>
+					)}
 				</div>
-
-
-				{/* <div className="flex flex-col h-[80vh]  bg-background">
-					<div className="flex items-center justify-between border-b px-4 py-3">
-						<h2 className="font-semibold">Messages </h2>
-					</div>
-
-					{
-						connections?.map((connection: any, i: number) => (
-							<div key={i} className="border-y-2 p-2 font-nunito font-bold cursor-pointer hover:bg-accent"
-								onClick={
-									() => handleConnectionConnect(connection.connected_with.username)
-								}>
-								{connection.connected_with.username}
-							</div>
-						))
-					}
-				</div>
-				<div className="flex flex-col h-[80vh] max-w-md mx-auto border rounded-lg shadow bg-background">
-					<div className="flex items-center justify-between  px-4 py-3">
-						<h2 className="font-semibold">Chat with {recipientId || "?"} </h2>
-						<h2 className="font-semibold">{userData.username}<ThemeToggle /></h2>
-					</div>
-
-					<div className="flex-1 overflow-y-auto px-3 py-2 space-y-2">
-						{messages.map((msg: any, i) => {
-							const isUser = msg.username === userData.username;
-							return (
-								<div
-									key={i}
-									className={`flex ${isUser ? "justify-end" : "justify-start"}`}
-								>
-									<div
-										className={`max-w-[70%] rounded-2xl px-3 py-2 text-sm ${isUser
-											? "bg-blue-500 text-white rounded-br-none"
-											: "bg-gray-200 text-gray-800 rounded-bl-none"
-											}`}
-									>
-										<p>{msg.message}</p>
-										{msg.timestamp && (
-											<span className="text-[10px] block mt-1 opacity-70 text-right">
-												{new Date(msg.timestamp).toLocaleTimeString([], {
-													hour: "2-digit",
-													minute: "2-digit",
-												})}
-											</span>
-										)}
-									</div>
-								</div>
-							);
-						})}
-						<div ref={messageEndRef} />
-					</div>
-
-					<div className="border-t px-3 py-2 flex items-center gap-2">
-						{!socketUrl ? (
-							<>
-								<TextField
-									label="Recipient ID"
-									type="text"
-									onChange={(e) => setRecipientId(e.target.value)}
-								/>
-								<Button onClick={handleConnect}>Start Chat</Button>
-							</>
-						) : (
-							<>
-								<input
-									type="text"
-									placeholder="Type a message..."
-									value={message}
-									onChange={(e) => setMessage(e.target.value)}
-									onKeyDown={(e) => e.key === "Enter" && handleMessageSend()}
-									className="flex-1 rounded-full border border-gray-300 px-4 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-blue-400"
-								/>
-								<Button onClick={handleMessageSend}>Send</Button>
-							</>
-						)}
-					</div>
-				</div> */}
 			</div>
 		</div>
 
